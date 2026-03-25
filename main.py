ecc741dd76cb49e275eef4a007adf41ea4297f44
Sidney <siddodson@gmail.com>
2026-03-25 17:30:43 -0400

    Make game on-going until done

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 board = ["1","2","3","4","5","6","7","8","9"]
+done = False
 
-
-print(board[0], "|", board[1], "|", board[2])
-print("---------")
-print(board[3], "|", board[4], "|", board[5])
-print("---------")
-print(board[6], "|", board[7], "|", board[8])
+def print_board():
+    print(board[0], "|", board[1], "|", board[2])
+    print("---------")
+    print(board[3], "|", board[4], "|", board[5])
+    print("---------")
+    print(board[6], "|", board[7], "|", board[8])
 
 print('...')
 
+# Starting statement
 print('Welcome to Tic-tac-toe')
 print('...')
 print('...')
 print('...')
 print('...')
 print('...')
-user_input = input('Where do you want to place your next Tic-tac-toe move? Choose between 1-9: ')
 
-board[int(user_input)-1] = 'X'
-print(board)
-
-
-print(board[0], "|", board[1], "|", board[2])
-print("---------")
-print(board[3], "|", board[4], "|", board[5])
-print("---------")
-print(board[6], "|", board[7], "|", board[8])
+while(not done):
+    user_input = input('Where do you want to place your next Tic-tac-toe move? Choose between 1-9: ')
+    board[int(user_input)-1] = 'X'
+    print_board()
