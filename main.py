5e44cbd826b719929d748f77688acb76fb827104
Sidney <siddodson@gmail.com>
2026-03-26 08:47:29 -0400

    Create input validation

--- main.py
+++ main.py
@@ -8,11 +8,20 @@ def print_board():
     print("---------")
     print(board[6], "|", board[7], "|", board[8])
 
-def spot_used_check(arg, game):
+def check_if_spot_filled(arg, game):
     if game[int(arg)-1] != 'X':
         return True
     else:
         print('_____That spot is already used!!!! Select another spot!!!!')
+        return False
+
+def input_validation(arg):
+    if arg.isdigit():
+        if(int(arg) > 9):
+            return False
+        else:
+            return True
+    else:
         return False
 
 print('...')
@@ -30,7 +39,13 @@ print('...')
 # Game condition
 while(not done):
     user_input = input('Where do you want to place your next Tic-tac-toe move? Choose between 1-9: ')
-    if (spot_used_check(user_input, board)):
-        board[int(user_input)-1] = 'X'
+    if (input_validation(user_input)):
+        if(check_if_spot_filled(user_input, board)):
+            board[int(user_input)-1] = 'X'
+        else:
+            print('hi')
+    else:
+        print('Input is not a valid number!')
+
 
     print_board()
