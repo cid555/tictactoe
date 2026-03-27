f45036768ef046c838515680a2751cf53b79803c
Sidney <siddodson@gmail.com>
2026-03-27 14:54:55 -0400

    Add game finish check

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ console.log("JS connected")
 
 let board = ["1","2","3","4","5","6","7","8","9"];
 let currentPlayer = "X";
+let done = false;
+
+function checkWinner() {
+    if (board[0] === board[1] && board[1] === board[2]) {
+        return true;
+    }
+    if (board[3] === board[4] && board[4] === board[5]) {
+        return true;
+    }
+    if (board[6] === board[7] && board[7] === board[8]) {
+        return true;
+    }
+    if (board[0] === board[3] && board[3] === board[6]) {
+        return true;
+    }
+    if (board[1] === board[4] && board[4] === board[7]) {
+        return true;
+    }
+    if (board[2] === board[5] && board[5] === board[8]) {
+        return true;
+    }
+    if (board[0] === board[4] && board[4] === board[8]) {
+        return true;
+    }
+    if (board[2] === board[4] && board[4] === board[6]) {
+        return true;
+    }
+    return false;
+}
 
 const cells = document.querySelectorAll(".cell");
 
@@ -14,6 +43,11 @@ cells.forEach(function(cell) {
 
             cell.textContent = currentPlayer;
             board[index] = currentPlayer;
+
+            if (checkWinner()) {
+                console.log(currentPlayer + " wins!");
+            }
+
             console.log(board)
             if (currentPlayer === "X") {
                 currentPlayer = "O";
