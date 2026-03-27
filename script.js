0789e7eec06255108f09f8e4e3db79fbc422df39
Sidney <siddodson@gmail.com>
2026-03-27 13:50:13 -0400

    Make button change based off variable

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 console.log("JS connected")
 
+let currentPlayer = "X";
+
 const button1 = document.getElementById("btn1");
 
 button1.addEventListener("click", function() {
     if (button1.textContent === "1") {
-        button1.textContent = "X";
+        button1.textContent = currentPlayer;
     }
 });
