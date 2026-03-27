94dc444d3aecc830d59a781bec852f1619e749d9
Sidney <siddodson@gmail.com>
2026-03-27 13:43:13 -0400

    Make button 1 turn into X

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("JS connected")
+
+const button1 = document.getElementById("btn1");
+
+button1.addEventListener("click", function() {
+    if (button1.textContent === "1") {
+        button1.textContent = "X";
+    }
+});
