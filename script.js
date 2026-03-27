6c673bdafde93fef080a6d5bbe07e1fabdf33977
Sidney <siddodson@gmail.com>
2026-03-27 14:43:30 -0400

    Change the list/array

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 console.log("JS connected")
 
+let board = ["1","2","3","4","5","6","7","8","9"];
 let currentPlayer = "X";
 
 const cells = document.querySelectorAll(".cell");
 
 cells.forEach(function(cell) {
     cell.addEventListener("click", function() {
-        if (cell.textContent !== "X" && cell.textContent !== "O") {
-            cell.textContent = currentPlayer;
 
+        const index = cell.dataset.index;
+
+        if (cell.textContent !== "X" && cell.textContent !== "O") {
+
+            cell.textContent = currentPlayer;
+            board[index] = currentPlayer;
+            console.log(board)
             if (currentPlayer === "X") {
                 currentPlayer = "O";
             } else {
